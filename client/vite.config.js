7f0036ddc0692528aa43f5d00094f589ab8391a1
Md Arwaz <mdarwaznew2017@gmail.com>
2025-12-01 17:23:15 +0530

    change in vite file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
+// import fs from 'fs';
 
 export default defineConfig({
   base: "/",
   server: {
     host: true,
-    https: {
-      key: fs.readFileSync("./key.pem"),
-      cert: fs.readFileSync("./cert.pem"),
-    },
+    // https: {
+    //   key: fs.readFileSync("./key.pem"),
+    //   cert: fs.readFileSync("./cert.pem"),
+    // },
     proxy: {
       "/api": "http://localhost:8080",
     },
