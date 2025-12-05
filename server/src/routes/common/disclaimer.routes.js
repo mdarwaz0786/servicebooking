03546ba82127dfd042d482c09beb4419d4829d73
Md Arwaz <mdarwaznew2017@gmail.com>
2025-12-05 16:08:54 +0530

    fix bug in diclaimer api

--- server/src/routes/common/disclaimer.routes.js
+++ server/src/routes/common/disclaimer.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import isLoggedIn from "../../middlewares/admin/auth.middleware.js";
 import { getDisclaimerById, getDisclaimers } from "../../controllers/common/disclaimer.controller.js";
 
 const router = express.Router();
 
-router.get("/", isLoggedIn, getDisclaimers);
-router.get("/:id", isLoggedIn, getDisclaimerById);
+router.get("/", getDisclaimers);
+router.get("/:id", getDisclaimerById);
 
 export default router;
