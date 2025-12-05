65aaf6b877dcc469ef6d56f8c820eb1d018a448a
Md Arwaz <mdarwaznew2017@gmail.com>
2025-12-05 13:02:28 +0530

    implement update profile api

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -46,6 +46,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    dob: {
+      type: String,
+      required: false,
+    }
   },
   { timestamps: true },
 );
