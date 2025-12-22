c1ee0fa7cd8dc9fc76b4c8a629d7424f92c16df9
shahrukhkhanektamek <sharukhkhanektamek1998@gmail.com>
2025-12-22 16:26:22 +0530

    fasfsa

--- server/src/controllers/serviceman/servicemanBooking.controller.js
+++ server/src/controllers/serviceman/servicemanBooking.controller.js
@@ -197,7 +197,7 @@ export const getServiceManBookingById = asyncHandler(async (req, res) => {
           populate: {
             path: "service",
             model: "Service",
-            select: "name image",
+            select: "name image categoryId subCategoryId",
             strictPopulate: false,
           },
         },
